bb9723424a26051ee572635a09d177b25d7517f6
Estanislao Crivos <estanislaocrivos@gmail.com>
2023-12-16 19:03:04 -0300

    Primera version del manejo de GPIO como objetos. Modificaciones menores al resto de los archivos.

--- src/gpio.c
+++ src/gpio.c
@@ -21,29 +21,53 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 SPDX-License-Identifier: MIT
 *************************************************************************************************/
 
-/** @file
- ** @brief Definición de la función principal del programa
+/** @file gpio.c
+ ** @brief Capa de abstracción para gestionar puertos de E/S digitales
+ *(implementación)
  **/
 
 /* === Headers files inclusions
  * =============================================================== */
 
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "gpio.h"
 
 /* === Macros definitions
  * ====================================================================== */
-#define LED_RED_PORT 3
-#define LED_RED_PIN 4
+
+#ifdef USE_STATIC_ALLOCATION
+#ifndef MAX_GPIO_INSTANCES
+#define MAX_GPIO_INSTANCES 16
+#endif
+#endif
 
 /* === Private data type declarations
  * ========================================================== */
+
+/// @brief Estructura con los atributos de un puerto
+struct GPIO {
+  uint32_t port;  //!< Número de puerto GPIO.
+  uint32_t pin;   //!< Pin del puerto GPIO.
+  bool is_output; //!< El puerto está configurado como salida.
+  bool state;     //!< Estado lógico del puerto
+#ifdef USE_STATIC_ALLOCATION
+  bool used; //!< El descriptor del puerto está ocupado.
+#endif
+};
 
 /* === Private variable declarations
  * =========================================================== */
 
 /* === Private function declarations
  * =========================================================== */
+
+#ifdef USE_STATIC_ALLOCATION
+/// @brief Función para alocar estáticamente un puerto GPIO.
+/// @return Puntero al objeto alocado si hay espacio, o NULL en caso contrario.
+static GPIO_t gpio_allocate();
+#endif
 
 /* === Public variable definitions
  * ============================================================= */
@@ -54,21 +78,59 @@ SPDX-License-Identifier: MIT
 /* === Private function implementation
  * ========================================================= */
 
+#ifdef USE_STATIC_ALLOCATION
+static GPIO_t gpio_allocate() {
+  static struct GPIO GPIO_INSTANCES[MAX_GPIO_INSTANCES] = {0};
+  GPIO_t ret = NULL;
+
+  for (size_t index = 0; index < MAX_GPIO_INSTANCES; index++) {
+    if (!GPIO_INSTANCES[index].used) {
+      ret = &GPIO_INSTANCES[index];
+      ret->used = true;
+      break;
+    }
+  }
+
+  return ret;
+}
+#endif
+
 /* === Public function implementation
  * ========================================================== */
 
-int main(void) {
-  // Creo el handler para el led_rojo
-  GPIO_t led_rojo = gpio_create(LED_RED_PORT, LED_RED_PIN, true);
+GPIO_t gpio_create(uint32_t port, uint32_t pin, bool is_output) {
 
-  // Configuro la direccion del GPIO (output)
-  gpio_set_direction(led_rojo, true);
+  GPIO_t self;
 
-  // Establezco el estado del GPIO (true)
-  gpio_set_state(led_rojo, true);
+#ifdef USE_STATIC_ALLOCATION
+  self = gpio_allocate();
+#else
+  self = malloc(sizeof(struct GPIO));
+#endif
 
-  return 0;
+  if (self) {
+    self->pin = pin;
+    self->port = port;
+    self->is_output = is_output;
+  }
+
+  printf("[gpio] Alocado Puerto <%d,%d> como %s\n", self->port, self->pin,
+         self->is_output ? "salida" : "entrada");
+  return self;
 }
+
+void gpio_set_direction(GPIO_t gpio, bool output) { gpio->is_output = output; }
+
+bool gpio_get_direction(GPIO_t gpio) { return gpio->is_output; }
+
+void gpio_set_state(GPIO_t gpio, bool state) {
+  if (gpio->is_output) {
+    gpio->state = state;
+    printf("[gpio] Puerto <%d,%d> = %d\n", gpio->port, gpio->pin, gpio->state);
+  }
+}
+
+bool gpio_get_state(GPIO_t gpio) { return gpio->state; }
 
 /* === End of documentation
  * ==================================================================== */
